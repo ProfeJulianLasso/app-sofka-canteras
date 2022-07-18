f28088e9b663b7cb944c0f9a2d9ffa1f6fe13e1a
Julian Lasso <98534497+ProfeJulianLasso@users.noreply.github.com>
2022-07-17 21:32:19 -0500

    Tamaño de letra en lectura un poco más grande, scroll suave cuando se cambia de página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'enabled'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
